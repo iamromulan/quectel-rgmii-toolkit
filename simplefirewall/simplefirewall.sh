76880055e5a127d520dc661f1856223e88f11fa9
iamromulan <50184035+iamromulan@users.noreply.github.com>
2024-01-02 00:48:01 -0500

    Update simplefirewall.sh
    Reorder commands

--- simplefirewall/simplefirewall.sh
+++ simplefirewall/simplefirewall.sh
@@ -5,8 +5,8 @@ PORTS=("80" "8080" "8088" "443") # Default ports, will be modified by the instal
 
 # Block specified ports on all interfaces except bridge0, eth0, and tailscale0
 for port in "${PORTS[@]}"; do
+    iptables -A INPUT -p tcp --dport $port -j DROP
     iptables -A INPUT -i bridge0 -p tcp --dport $port -j ACCEPT
     iptables -A INPUT -i eth0 -p tcp --dport $port -j ACCEPT
     iptables -A INPUT -i tailscale0 -p tcp --dport $port -j ACCEPT
-    iptables -A INPUT -p tcp --dport $port -j DROP
 done
