3c342ff6fc76274074676742824f4b9d01ffa425
iamromulan <50184035+iamromulan@users.noreply.github.com>
2024-01-05 18:24:31 -0500

    Update simplefirewall.sh

--- simplefirewall/simplefirewall.sh
+++ simplefirewall/simplefirewall.sh
@@ -3,10 +3,14 @@
 # Define the ports you want to block
 PORTS=("80" "8080" "8088" "443") # Default ports, will be modified by the install script
 
-# Block specified ports on all interfaces except bridge0, eth0, and tailscale0
+# First, allow specified ports on bridge0, eth0, and tailscale0
 for port in "${PORTS[@]}"; do
-    iptables -A INPUT -p tcp --dport $port -j DROP
     iptables -A INPUT -i bridge0 -p tcp --dport $port -j ACCEPT
     iptables -A INPUT -i eth0 -p tcp --dport $port -j ACCEPT
     iptables -A INPUT -i tailscale0 -p tcp --dport $port -j ACCEPT
+done
+
+# Then, block specified ports on all other interfaces
+for port in "${PORTS[@]}"; do
+    iptables -A INPUT -p tcp --dport $port -j DROP
 done
