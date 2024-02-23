551ab341bcfced38d16fd96775b0800bab2ce568
iamromulan <50184035+iamromulan@users.noreply.github.com>
2024-02-22 22:44:34 -0500

    Revert "Fix TTL Update from toolkit"
    This reverts commit 9ec5223806a7cfbe111fc73ca932eaf9c2276692.

--- RMxxx_rgmii_toolkit.sh
+++ RMxxx_rgmii_toolkit.sh
@@ -273,7 +273,6 @@ configure_simple_firewall() {
             return
         else
             echo "$new_ttl_value" > /usrdata/simplefirewall/ttlvalue
-	    systemctl restart ttl-override
             echo "TTL value updated to $new_ttl_value."
         fi
         ;;
