3fce9a345514f8073947d03fa738fbe9e2959c42
CHIENNT97 <151820989+CHIENNT97@users.noreply.github.com>
2024-06-19 19:09:42 -0700

    Update RMxxx_rgmii_toolkit.sh

--- RMxxx_rgmii_toolkit.sh
+++ RMxxx_rgmii_toolkit.sh
@@ -2,7 +2,7 @@
 
 # Define toolkit paths
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/opt/bin:/opt/sbin:/usrdata/root/bin
-GITUSER="iamromulan"
+	GITUSER="CHIENNT97"
 GITTREE="development"
 GITMAINTREE="main"
 GITDEVTREE="development"
