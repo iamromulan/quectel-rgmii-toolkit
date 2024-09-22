5d92fdeadf4076012a7c886686d11774126afcf1
Cameron Thompson <50184035+iamromulan@users.noreply.github.com>
2024-09-22 05:08:27 -0400

    Progress
    Reorganize; add a new service that actively keeps rc.d in sync with the real rootfs

--- rcPCIe_SDXPINN_toolkit.sh
+++ rcPCIe_SDXPINN_toolkit.sh
@@ -1,24 +1,18 @@
-#!/bin/sh
+#!/bin/ash
 
 #WORK IN PROGRESS
 
 # Define toolkit paths
 GITUSER="iamromulan"
-GITTREE="development-RM551"
-GITMAINTREE="main"
-GITDEVTREE="development"
+GITREPO="quectel-rgmii-toolkit"
+GITTREE="SDXPINN"
+GITMAINTREE="SDXPINN"
+GITDEVTREE="development-SDXPINN"
 TMP_DIR="/tmp"
 USRDATA_DIR="/data"
-SOCAT_AT_DIR="/data/socat-at-bridge"
-SOCAT_AT_SYSD_DIR="/data/socat-at-bridge/systemd_units"
-SIMPLE_ADMIN_DIR="/data/simpleadmin"
 SIMPLE_FIREWALL_DIR="/data/simplefirewall"
 SIMPLE_FIREWALL_SCRIPT="$SIMPLE_FIREWALL_DIR/simplefirewall.sh"
 SIMPLE_FIREWALL_SYSTEMD_DIR="$SIMPLE_FIREWALL_DIR/systemd"
-
-# AT Command Script Variables and Functions
-DEVICE_FILE="/dev/smd7"
-TIMEOUT=4  # Set a timeout for the response
 
 # Function to remount file system as read-write
 remount_rw() {
@@ -30,76 +24,23 @@ remount_ro() {
     mount -o remount,ro /
 }
 
-# Basic AT commands without socat bridge for fast response commands only
-start_listening() {
-    cat "$DEVICE_FILE" > /tmp/device_readout &
-    CAT_PID=$!
-}
-
-send_at_command() {
-    echo -e "\e[1;31mThis only works for basic quick responding commands!\e[0m"  # Red
-    echo -e "\e[1;36mType 'install' to simply type atcmd in shell from now on\e[0m"
-    echo -e "\e[1;36mThe installed version is much better than this portable version\e[0m"
-    echo -e "\e[1;32mEnter AT command (or type 'exit' to quit): \e[0m"
-    read at_command
-    if [ "$at_command" = "exit" ]; then
-        return 1
-    fi
-    
-    if [ "$at_command" = "install" ]; then
-        install_update_at_socat
-        echo -e "\e[1;32mInstalled. Type atcmd from adb shell or ssh to start an AT Command session\e[0m"
-        return 1
-    fi
-    echo -e "${at_command}\r" > "$DEVICE_FILE"
-}
-
-wait_for_response() {
-    local start_time=$(date +%s)
-    local current_time
-    local elapsed_time
-
-    echo -e "\e[1;32mCommand sent, waiting for response...\e[0m"
+send_at_commands_using_atcmd() {
     while true; do
-        if grep -qe "OK" -e "ERROR" /tmp/device_readout; then
-            echo -e "\e[1;32mResponse received:\e[0m"
-            cat /tmp/device_readout
-            return 0
+        echo -e "\e[1;32mEnter AT command (or type 'exit' to return to the main menu): \e[0m"
+        read at_command
+        if [ "$at_command" = "exit" ]; then
+            echo -e "\e[1;32mReturning to the main menu.\e[0m"
+            break
         fi
-        current_time=$(date +%s)
-        elapsed_time=$((current_time - start_time))
-        if [ "$elapsed_time" -ge "$TIMEOUT" ]; then
-            echo -e "\e[1;31mError: Response timed out.\e[0m"  # Red
-            echo -e "\e[1;32mIf the response takes longer than a second or 2 to respond this will not work\e[0m"  # Green
-            echo -e "\e[1;36mType install to install the better version of this that will work.\e[0m"  # Cyan
-            return 1
-        fi
-        sleep 1
+        echo -e "\e[1;32mSending AT command: $at_command\e[0m"
+        echo -e "\e[1;32mResponse:\e[0m"
+        # Use atcmd to send the command and display the output
+        atcmd_output=$(atcmd "$at_command")
+        echo "$atcmd_output"
+        echo -e "\e[1;32m----------------------------------------\e[0m"
     done
 }
 
-cleanup() {
-    kill "$CAT_PID"
-    wait "$CAT_PID" 2>/dev/null
-    rm -f /tmp/device_readout
-}
-
-send_at_commands() {
-    if [ -c "$DEVICE_FILE" ]; then
-        while true; do
-            start_listening
-            send_at_command
-            if [ $? -eq 1 ]; then
-                cleanup
-                break
-            fi
-            wait_for_response
-            cleanup
-        done
-    else
-        echo -e "\e[1;31mError: Device $DEVICE_FILE does not exist!\e[0m"
-    fi
-}
 
 overlay_check() {
     if ! grep -qs '/real_rootfs ' /proc/mounts; then
@@ -131,16 +72,20 @@ basic_55x_setup() {
     fi
 
     cd /etc/init.d/
-    wget https://raw.githubusercontent.com/iamromulan/quectel-rgmii-toolkit/development-SDXPINN/init.d/mount-fix
-    # wget https://raw.githubusercontent.com/iamromulan/quectel-rgmii-toolkit/development-SDXPINN/init.d/init-overlay-watchdog
+    wget https://raw.githubusercontent.com/$GITUSER/$GITREPO/$GITTREE/etc/init.d/mount-fix
+    wget https://raw.githubusercontent.com/$GITUSER/$GITREPO/$GITTREE/etc/init.d/init-overlay-watchdog
     # Set executable permissions
     chmod +x mount-fix
-    # chmod +x init-overlay-watchdog
-    cd /
+    chmod +x init-overlay-watchdog
+    cd /usr/sbin
+	wget https://raw.githubusercontent.com/$GITUSER/$GITREPO/$GITTREE/usr/sbin/init-overlay-watchdog.sh
+	cd /
     service mount-fix enable
-    # service init-overlay-watchdog enable
+    service init-overlay-watchdog enable
     service mount-fix start
-    # service init-overlay-watchdog start
+	opkg update
+	opkg opkg install inotifywait inotifywatch
+    service init-overlay-watchdog start
     echo -e "\e[92m"
     echo "Mount fix completed!"
     echo "Visit https://github.com/iamromulan for more!"
@@ -218,6 +163,9 @@ ttl_setup() {
   done
 }
 
+set_root_passwd() {
+    passwd
+}
 
 # Function for Tailscale Submenu
 tailscale_menu() {
@@ -239,12 +187,67 @@ tailscale_menu() {
 
 # Function to install, update, or remove Tailscale
 install_update_tailscale() {
-    echo -e "\e[1;31m2) Installing tailscale from opkg\e[0m"
+    echo -e "\e[1;31mInstalling Tailscale from opkg...\e[0m"
     opkg install tailscale
-    echo -e "\e[1;32mTailscale has been updated/installed.\e[0m"
-    echo -e "\e[1;31mTailscale is not up to date!.\e[0m"
-    echo -e "\e[1;32mReplace the tailscale and tailscaled binaries with the new ones and run tailscale update.\e[0m"
+    if [ $? -ne 0 ]; then
+        echo -e "\e[1;31mFailed to install Tailscale via opkg.\e[0m"
+        return 1
+    fi
+
+    echo -e "\e[1;32mTailscale has been installed via opkg.\e[0m"
+    echo -e "\e[1;32mUpdating to the latest Tailscale version...\e[0m"
+	
+	# Stop Running Service
+	service tailscale stop
+
+    # Define variables for the download
+    TAILSCALE_URL="https://pkgs.tailscale.com/stable/tailscale_1.74.1_arm64.tgz"
+    TAILSCALE_TGZ="/tmp/tailscale_1.74.1_arm64.tgz"
+    TAILSCALE_TMP_DIR="/tmp/tailscale_update"
+
+    # Download the latest Tailscale package
+    echo -e "\e[1;32mDownloading latest Tailscale package...\e[0m"
+    wget -O "$TAILSCALE_TGZ" "$TAILSCALE_URL"
+    if [ $? -ne 0 ]; then
+        echo -e "\e[1;31mFailed to download Tailscale package. Please check your internet connection.\e[0m"
+        rm -f "$TAILSCALE_TGZ"
+        return 1
+    fi
+
+    # Extract the package
+    echo -e "\e[1;32mExtracting Tailscale package...\e[0m"
+    mkdir -p "$TAILSCALE_TMP_DIR"
+    tar -xzf "$TAILSCALE_TGZ" -C "$TAILSCALE_TMP_DIR"
+    if [ $? -ne 0 ]; then
+        echo -e "\e[1;31mFailed to extract Tailscale package.\e[0m"
+        rm -f "$TAILSCALE_TGZ"
+        rm -rf "$TAILSCALE_TMP_DIR"
+        return 1
+    fi
+
+    # Replace the binaries with force option
+    echo -e "\e[1;32mUpdating Tailscale binaries...\e[0m"
+    cp -f "$TAILSCALE_TMP_DIR/tailscale_1.74.1_arm64/tailscale" /usr/sbin/
+    cp -f "$TAILSCALE_TMP_DIR/tailscale_1.74.1_arm64/tailscaled" /usr/sbin/
+    if [ $? -ne 0 ]; then
+        echo -e "\e[1;31mFailed to copy new Tailscale binaries.\e[0m"
+        rm -f "$TAILSCALE_TGZ"
+        rm -rf "$TAILSCALE_TMP_DIR"
+        return 1
+    fi
+
+    # Set the correct permissions
+    chmod +x /usr/sbin/tailscale /usr/sbin/tailscaled
+
+    # Clean up temporary files
+    rm -f "$TAILSCALE_TGZ"
+    rm -rf "$TAILSCALE_TMP_DIR"
+
+    # Start Tailscale service if available
+    service tailscale start
+    echo -e "\e[1;32mTailscale version 1.74.1 installed\e[0m"
 }
+
 
 # Function to Configure Tailscale
 configure_tailscale() {
@@ -353,7 +356,7 @@ while true; do
     read -p "Enter your choice: " choice
 
     case $choice in
-        1) send_at_commands ;;
+        1) send_at_commands_using_atcmd ;;
         2) remount_rw; basic_55x_setup ;;
         3) 
             overlay_check
