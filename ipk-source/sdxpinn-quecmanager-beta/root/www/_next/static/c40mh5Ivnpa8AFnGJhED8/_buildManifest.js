46f8e1af8b85156597f5f2bc5e568498f6fb85ef
Cameron Thompson <50184035+iamromulan@users.noreply.github.com>
2024-12-11 23:00:57 -0500

    QuecManger 1.0.0-beta
    Release Notes:
    
    Enhanced Web App Architecture:
    
    - Optimized the structure for better scalability and maintainability.
    
    - Refined Data Fetching Logic:
    Improved the efficiency and reliability of data retrieval processes.
    
    - Optimized AT Terminal: Enhanced functionality for smoother interaction with AT commands.
    
    - Revamped UI/UX Design: Streamlined user interface and experience for improved accessibility and usability.
    
    New Features:
    
    - MTU Settings: Added customizable Maximum Transmission Unit (MTU) settings.
    
    - APN and IMEI Profiles: Introduced the ability to configure APN and IMEI profiles for better device management.
    
    - Persistent Cell Locking: Added native support for persistent cell locking, ensuring consistent network connections.
    
    - QuecWatch: Connection monitoring with auto reconnect and reboot settable
    
    Co-Authored-By: Russel Yasol <73575327+dr-dolomite@users.noreply.github.com>

--- ipk-source/sdxpinn-quecmanager-beta/root/www/_next/static/c40mh5Ivnpa8AFnGJhED8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-0eb3e56ba33eb808.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
