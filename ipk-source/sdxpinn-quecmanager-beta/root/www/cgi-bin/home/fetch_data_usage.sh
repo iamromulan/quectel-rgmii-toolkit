397e0de45a7e4842b3d3267cb28d24e7e2dd15d4
Russel Yasol <zxc.11rus@gmail.com>
2025-01-17 14:29:29 +0800

    Ready for version 1.1.2
    PR for the main repository, version 1.1.2 with sms_tool as the major change

--- ipk-source/sdxpinn-quecmanager-beta/root/www/cgi-bin/home/fetch_data_usage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Set content type to JSON
-echo "Content-Type: application/json"
-echo
-
-# Read the JSON file and get only the last entry using jq
-jq 'last' /tmp/signal_graphs/data_usage.json
